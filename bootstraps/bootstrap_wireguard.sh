15f40a6213c639a921e51f218c96bf5e64ffd07b
sasabosnic <30678188+sasabosnic@users.noreply.github.com>
2020-11-29 11:04:15 +0100

    Update bootstrap_wireguard.sh

--- bootstraps/bootstrap_wireguard.sh
+++ bootstraps/bootstrap_wireguard.sh
@@ -1,11 +1,11 @@
 # Bootstrap the system
-ftp -V -o - https://github.com/findelabs/openbsd-ansible-deploy/raw/master/bootstraps/bootstrap_raw.sh | sh
+ftp -V -o - https://github.com/sasabosnic/openbsd-ansible-deploy/raw/master/bootstraps/bootstrap_raw.sh | sh
 
 # Extra variables for playbook
 extra_vars="\
 role_sysctl_task=router_sysctl \
 unbound_address=10.10.0.1 \
-vnstatd_interface=tun3"
+vnstatd_interface=wg0"
 
 # Run playbook
-cd /root/git/openbsd-ansible-deploy/ && ansible-playbook install.yml --tags=users,system,wireguard-pf,wireguard-binaries,wireguard-configs,unbound,vnstatd,ifstated,sysctl --extra-vars="$extra_vars"
+cd /root/git/openbsd-ansible-deploy/ && ansible-playbook install.yml --tags=users,system,wireguard-pf,wireguard-configs,unbound,vnstatd,ifstated,sysctl --extra-vars="$extra_vars"
